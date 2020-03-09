9ce3c3aa37db8665f1fc570feed485dac8c156bd
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2020-03-09 12:54:13 +0530

    Update AppApplication.java

--- app/src/main/java/com/IRecruiter/app/AppApplication.java
+++ app/src/main/java/com/IRecruiter/app/AppApplication.java
@@ -8,7 +8,7 @@ public class AppApplication {
 
 	public static void main(String[] args)
 	{
-		System.out.println("I-Recuiter");
+		System.out.println("I-Recuiter ");
 		SpringApplication.run(AppApplication.class, args);
 	}
 
