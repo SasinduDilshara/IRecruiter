2385af4a416eab82ca136ce565e1fc2fae0c4bee
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2020-03-09 12:53:24 +0530

    update file structure and test

--- app/src/main/java/com/IRecruiter/app/AppApplication.java
+++ app/src/main/java/com/IRecruiter/app/AppApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AppApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
+		System.out.println("I-Recuiter");
 		SpringApplication.run(AppApplication.class, args);
 	}
 
